e5e6270bf2eb129045b638ac4350a527cd9b44a5
Rakholiya Jenish <rakholiyajenish.07@gmail.com>
2015-11-26 02:06:49 +0530

    autopep8

--- setup.py
+++ setup.py
@@ -22,12 +22,12 @@
 
 from distutils.core import setup, Extension
 
-module = Extension('demangler',sources=['demangle.cpp'])
-setup  (name = "Demangler",
-        version = "0.1",
-        description = "A demangler package for gcc 3.x and gcc 4.x",
-        author = "Rakholiya Jenish",
-        author_email = "rakholiyajenish.07@gmail.com",
-        keywords = "demangle gcc",
-        license = "MIT",
-        ext_modules = [module])
+module = Extension('demangler', sources=['demangle.cpp'])
+setup(name="Demangler",
+      version="0.1",
+      description="A demangler package for gcc 3.x and gcc 4.x",
+      author="Rakholiya Jenish",
+      author_email="rakholiyajenish.07@gmail.com",
+      keywords="demangle gcc",
+      license="MIT",
+      ext_modules=[module])
