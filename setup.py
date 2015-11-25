cc0134c62767d837f399a2f287dcc5830335531e
Rakholiya Jenish <rakholiyajenish.07@gmail.com>
2015-11-26 00:47:16 +0530

    update description and keywords

--- setup.py
+++ setup.py
@@ -25,8 +25,9 @@ from distutils.core import setup, Extension
 module = Extension('demangler',sources=['demangle.cpp'])
 setup  (name = "Demangler",
         version = "0.1",
-        description = "This is a demangle package",
+        description = "A demangler package for gcc 3.x and gcc 4.x",
         author = "Rakholiya Jenish",
         author_email = "rakholiyajenish.07@gmail.com",
+        keywords = "demangle gcc",
         license = "MIT",
         ext_modules = [module])
