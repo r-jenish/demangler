eec1f8feaf6720bec471c7bdfec57bf8961dc1cf
Rakholiya Jenish <rakholiyajenish.07@gmail.com>
2015-12-03 18:22:03 +0530

    update version, url and long description

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from distutils.core import setup, Extension
 
 module = Extension('_demangler', sources=['demangle.cpp'])
 setup(name="Demangler",
-      version="0.1",
+      version="0.2",
       description="A demangler package for gcc 3.x and gcc 4.x",
       author="Rakholiya Jenish",
       author_email="rakholiyajenish.07@gmail.com",
+      long_description=".. image:: https://travis-ci.org/P4N74/demangler.svg?branch=master\
+    :target: https://travis-ci.org/P4N74/demangler",
       keywords="demangle gcc",
       license="MIT",
       scripts=['demangler.py'],
+      url="https://github.com/P4N74/demangler",
       ext_modules=[module])
